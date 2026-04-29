5b21c791b459bf3cda8920c05dd0f7f65d9aae0b
syahrulhannan07 <syahrulhannan07@gmail.com>
2026-04-30 01:05:03 +0700

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_cofe_job/main.dart';
+import 'package:cofe_job/main.dart';
 
 void main() {
   testWidgets('App builds successfully', (WidgetTester tester) async {
     // Build our app
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CafeJobApp());
 
     // Verify that MaterialApp exists
     expect(find.byType(MaterialApp), findsOneWidget);
