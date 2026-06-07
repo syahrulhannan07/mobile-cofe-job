ead0b39b88746f7d5fc7210745d9c1dfcb9cabd3
syahrulhannan07 <syahrulhannan07@gmail.com>
2026-06-07 17:36:57 +0700

    Add release SHA1 Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,17 +6,6 @@ plugins {
     id("com.google.gms.google-services")
 }
 
-import java.util.Properties
-import java.io.FileInputStream
-
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-    println("KEYSTORE PATH = ${keystoreProperties["storeFile"]}")
-}
-
 android {
     namespace = "com.example.cofe_job"
     compileSdk = flutter.compileSdkVersion
@@ -42,20 +31,11 @@ android {
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-    create("release") {
-        keyAlias = keystoreProperties["keyAlias"] as String
-        keyPassword = keystoreProperties["keyPassword"] as String
-        storeFile = file(keystoreProperties["storeFile"] as String)
-        storePassword = keystoreProperties["storePassword"] as String
-        }
-    }
-
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
